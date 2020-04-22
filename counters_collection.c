326c3a75cf4aa29b899b797d0076afea68eb06d7
tshinhar <tshinhar@gmail.com>
2020-04-22 11:51:02 +0300

    fixed printing with A flag

--- counters_collection.c
+++ counters_collection.c
@@ -5,5 +5,6 @@ void reset_counters(Counters *counters){
     counters->line_counter=1;
     counters->bit_counter=0;
     counters->match_counter=0;
+    counters->a_block_counter=0;
 }
 
--- counters_collection.h
+++ counters_collection.h
@@ -4,6 +4,7 @@ typedef struct Counters{
     int line_counter;
     int bit_counter;
     int match_counter;
+    int a_block_counter;
 } Counters;
 
 void reset_counters(Counters *counters);
